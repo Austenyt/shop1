6f70300e19eae528be7e9eee6868faafb0664636
Austenyt <austenyt@gmail.com>
2024-02-05 17:01:11 +0300

    21.2 Forms

--- users/views.py
+++ users/views.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.views import LoginView as BaseLoginView
 from django.contrib.auth.views import LoginView as BaseLogoutView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView
+
+from users.models import User
 
 
 class LoginView(BaseLoginView):
@@ -8,3 +12,20 @@ class LoginView(BaseLoginView):
 
 class LogoutView(BaseLogoutView):
     template_name = 'users/login.html'
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
+
+
+class ProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    success_url = reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
